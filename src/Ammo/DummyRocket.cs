501b0cb281f78087b810cec5a5eb4c6542f6f91e
andrebq <andrebq@gmail.com>
2020-04-04 00:25:14 +0200

    Basic explosion after collision

--- src/Ammo/DummyRocket.cs
+++ src/Ammo/DummyRocket.cs
@@ -4,13 +4,14 @@ using System;
 public class DummyRocket : KinematicBody2D
 {
     private AnimationPlayer animation;
+    private Particles2D explosion;
+    private bool exploded;
+    private Node2D missileGraphics;
 
     [Export]
     public float Lifetime { get; set; }
-
     [Export]
     public Vector2 Heading { get; set; }
-
     [Export]
     public float Speed { get; set; }
 
@@ -24,6 +25,8 @@ public class DummyRocket : KinematicBody2D
     public override void _Ready()
     {
         animation = FindNode("Animation", true, true) as AnimationPlayer;
+        explosion = FindNode("Explosion") as Particles2D;
+        missileGraphics = FindNode("MissileGraphics") as Node2D;
     }
 
     public void Fire()
@@ -34,10 +37,12 @@ public class DummyRocket : KinematicBody2D
 
     public override void _Process(float delta)
     {
-        var collision = MoveAndCollide(Heading * Speed * delta);
-        if (collision != null)
-        {
-            Explode();
+        if (!exploded) {
+            var collision = MoveAndCollide(Heading * Speed * delta);
+            if (collision != null)
+            {
+                Explode();
+            }
         }
         Lifetime -= delta;
         if (Lifetime <= 0)
@@ -48,7 +53,10 @@ public class DummyRocket : KinematicBody2D
 
     public void Explode()
     {
-        Vanish();
+        exploded = true;
+        explosion.Emitting = true;
+        missileGraphics.Visible = false;
+        Lifetime = explosion.Lifetime;
     }
 
     public void Vanish()
